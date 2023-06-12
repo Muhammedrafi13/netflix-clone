11299f73810a52bf5ecf063fdeaa48678fd9c8d4
Muhammedrafi13 <muhammedrafif111@gmail.com>
2023-06-12 21:28:09 +0530

    add new feature

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import Banner from './Components/Banner/Banner';
 import NavBar from './Components/NavBar/NavBar';
 import RowPost from './Components/RowPost/RowPost';
-import { originals,actions } from './urls';
+import Footer from './Components/Footer/Footer'
+import { originals,actions, adventure, animation, comedy, crime, documentary, family, fantasy, horror, history } from './urls';
 
 
 function App() {
@@ -11,7 +12,17 @@ function App() {
      <NavBar/>
      <Banner/>
      <RowPost   title="Netflix Original" url={originals} /> 
-     <RowPost   title="Action" url={actions} isSmall /> 
+     <RowPost   title="Action" url={actions} isSmall />
+     <RowPost   title="Adventure" url={adventure} isSmall />
+     <RowPost   title="Animation" url={animation} isSmall />
+     <RowPost   title="Comedy" url={comedy} isSmall />
+     <RowPost   title="Crime" url={crime} isSmall />
+     <RowPost   title="Documentary" url={documentary} isSmall />
+     <RowPost   title="Family" url={family} isSmall />
+     <RowPost   title="Fantasy" url={fantasy} isSmall />
+     <RowPost   title="History" url={history} isSmall />
+     <RowPost   title="Horror" url={horror} isSmall />
+     <Footer/>
     </>
   );
 }
--- src/urls.js
+++ src/urls.js
@@ -2,3 +2,12 @@ import { API_KEY } from "./constants/constants";
 
 export const originals = `/discover/tv?api_key=${API_KEY}&with_networks=213`;
 export const actions = `/discover/movie?api_key=${API_KEY}&with_genres=28`;
+export const adventure = `discover/movie?api_key=${API_KEY}&with_genres=12`
+export const animation = `discover/movie?api_key=${API_KEY}&with_genres=16`
+export const comedy = `discover/movie?api_key=${API_KEY}&with_genres=35`
+export const crime = `discover/movie?api_key=${API_KEY}&with_genres=80`
+export const documentary = `discover/movie?api_key=${API_KEY}&with_genres=99`
+export const family = `discover/movie?api_key=${API_KEY}&with_genres=10751`
+export const fantasy = `discover/movie?api_key=${API_KEY}&with_genres=14`
+export const history = `discover/movie?api_key=${API_KEY}&with_genres=36`
+export const horror = `discover/movie?api_key=${API_KEY}&with_genres=27`
